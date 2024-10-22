5eebee3a84ea95bcf7ae05324c2a4f591961711a
tetew1205 <theo.abitbol@epitech.eu>
2024-10-22 10:25:06 +0200

    [ADD] css for modi_debiteurs

--- dreki_app/pages/mod_debiteurs/index.js
+++ dreki_app/pages/mod_debiteurs/index.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { useRouter } from 'next/router';
 import AddDebiteurLayout from './layout';
+import '../css/debiteurs.css'
 
 export default function ModiDebiteur() {
     const router = useRouter();
